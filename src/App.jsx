11d8ad60bef89aeefde3fb2240e49ec38dc41481
Adrian <adriancoceres.93@gmail.com>
2025-05-30 15:11:01 -0300

    add context

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useContext } from 'react'
 import './assets/style/style.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
@@ -12,75 +12,20 @@ import Cart from './components/Cart'
 import Admin from './pages/Admin'
 import Login from './pages/Login'
 import RutaProtegida from './Auth/RutasProtegidas'
-
+import { CartContext } from './context/cartContext'
 
 function App() {
 
-  const [cart, setCart] = useState([])
-  const [productos, setProductos] = useState([])
-  const [cargando, setCargando] = useState(true)
-  const [error, setError] = useState(false)
-  /* con esta funcion se comprueba si el usuario esta o no auth */
-  const [isAuthenticated, setIsAuth] = useState(true)
-
-  useEffect(() => {
-    fetch('/data/data.json')
-      .then(respuesta => respuesta.json())
-      .then(datos => {
-        setTimeout(() => {
-          setProductos(datos)
-          setCargando(false)
-        }, 2000)
-      })
-      .catch(error => {
-        console.log('Error', error)
-        setCargando(false)
-        setError(true)
-      })
-
-  }, [])
-
-  const handleAddToCart = (product, cantidad) => {
-
-    const productInCart = cart.find((item) => item.id === product.id);
-    if (productInCart) {
-
-      setCart(cart.map((item) => item.id === product.id ? { ...item, quantity: item.quantity + cantidad } : item));
-    } else {
-      setCart([...cart, { ...product, quantity: cantidad }]);
-    }
-  };
-
-  const handleDeleteFromCart = (product) => {
-    setCart(prevCart => {
-      return prevCart.map(item => {
-        if (item.id === product.id) {
-          if (item.quantity > 1) {
-            return { ...item, quantity: item.quantity - 1 };
-          } else {
-            return null; // Si quantity es 1, marcamos para eliminar
-          }
-        } else {
-          return item; // Si no es el producto, lo dejamos igual
-        }
-      }).filter(item => item !== null); // Quitamos los productos nulos
-    });
-  };
-
-
-
+  const { cart, productos, cargando, error, handleAddToCart, handleDeleteFromCart, isAuthenticated } = useContext(CartContext)
+  
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home borrarProducto={handleDeleteFromCart} agregarCarrito={handleAddToCart} cart={cart} productos={productos} cargando={cargando} />} />
-
-        <Route path='/acercade' element={<AcercaDe borrarProducto={handleDeleteFromCart} cart={cart} />} />
-
-        <Route path='/productos' element={<Galeria borrarProducto={handleDeleteFromCart} agregarCarrito={handleAddToCart} cart={cart} productos={productos} cargando={cargando} />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/acercade' element={<AcercaDe />} />
+        <Route path='/productos' element={<Galeria />} />
         <Route path='/productos/:id' element={<DetalleProductos productos={productos} />} />
         <Route path='/contacto' element={<Contacto borrarProducto={handleDeleteFromCart} cart={cart} />} />
-        <Route path='/cart' element={<RutaProtegida isAuthenticated={isAuthenticated}><Cart /></RutaProtegida>} />
-
         <Route path='/admin' element={<RutaProtegida isAuthenticated={isAuthenticated}> <Admin /></RutaProtegida>} />
         <Route path='/login' element={<Login />} />
         <Route path='*' element={<NotFound />} />
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,7 +1,65 @@
-import React, { useState } from 'react'
+import React, { Children, useState } from 'react'
+import Productos from '../components/Productos';
 
 export const CartContext = createContext();
 
-export function CartProvider({ children }) {
+export const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([])
+    const [productos, setProductos] = useState([])
+    const [cargando, setCargando] = useState(true)
+    const [error, setError] = useState(false)
+    /* con esta funcion se comprueba si el usuario esta o no auth */
+    const [isAuthenticated, setIsAuth] = useState(true)
+
+    useEffect(() => {
+        fetch('/data/data.json')
+            .then(respuesta => respuesta.json())
+            .then(datos => {
+                setTimeout(() => {
+                    setProductos(datos)
+                    setCargando(false)
+                }, 2000)
+            })
+            .catch(error => {
+                console.log('Error', error)
+                setCargando(false)
+                setError(true)
+            })
+
+    }, [])
+
+    const handleAddToCart = (product, cantidad) => {
+
+        const productInCart = cart.find((item) => item.id === product.id);
+        if (productInCart) {
+
+            setCart(cart.map((item) => item.id === product.id ? { ...item, quantity: item.quantity + cantidad } : item));
+        } else {
+            setCart([...cart, { ...product, quantity: cantidad }]);
+        }
+    };
+
+    const handleDeleteFromCart = (product) => {
+        setCart(prevCart => {
+            return prevCart.map(item => {
+                if (item.id === product.id) {
+                    if (item.quantity > 1) {
+                        return { ...item, quantity: item.quantity - 1 };
+                    } else {
+                        return null; // Si quantity es 1, marcamos para eliminar
+                    }
+                } else {
+                    return item; // Si no es el producto, lo dejamos igual
+                }
+            }).filter(item => item !== null); // Quitamos los productos nulos
+        });
+    };
+
+    return (
+        <CartContext.Provider
+            value={{ cart, productos, cargando, error, handleAddToCart, handleDeleteFromCart, isAuthenticated, setIsAuth }}>
+            {children}
+        </CartContext.Provider>
+    )
+
 }
